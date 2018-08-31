e922ecf17b51d08fed9fc0bcd760f6356e4be0d6
libicc <libicc@126.com>
2018-08-31 23:25:09 +0800

    removed the redundant data, add a random fator 1, use abs of the data to train, get a 91.67% accrate rate

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,21 @@ import os
 import numpy as np
 import scipy.io as sio
 import pandas as pd
-import random
+
+####################################################
+
+def matrix2vector(np_m):
+    vec = np.triu(np_m, 1).flatten()
+    vec_data = pd.DataFrame(vec)
+
+    to_drop = []
+    for i in np.arange(68):
+	    for j in np.arange(0,i+1):
+		    to_drop.append(i*68+j)
+    vec_data.drop(index=to_drop, inplace=True)
+    vec = np.array(vec_data).reshape([1,2278])
+    
+    return vec
 
 ####################################################
 #将正常数据读入内存,记为normal_data
@@ -12,15 +26,16 @@ print('***********************正常数据文件列表************************')
 print(file_list1)
 print(len(file_list1))
 
-normal_data = np.empty(shape=[57, 32368], dtype=np.complex128)
+normal_data = np.empty(shape=[57, 15946], dtype=np.complex128)
 for count, file in enumerate(file_list1):
     data = sio.loadmat(path1 + file)
     data.pop('__globals__'); data.pop('__header__');  data.pop('__version__')
+    sorted(data.keys())
 
     for i, fi in enumerate(data):
-        normal_data[count, i*4624:i*4624 + 4624] = data[fi].reshape([1,4624])
+       normal_data[count, i*2278:i*2278+2278] = matrix2vector(data[fi])
 
-print(normal_data[:,32367])
+print(normal_data[:,15945])
 np.save('normal_data.npy', normal_data)
 
 #############################################
@@ -31,25 +46,20 @@ print('***********************棘波数据文件列表************************')
 print(file_list1)
 print(len(file_list2))
 
-abnormal_data = np.empty(shape=[60, 32368], dtype=np.complex128)
+abnormal_data = np.empty(shape=[60, 15946], dtype=np.complex128)
 for count, file in enumerate(file_list2):
     data = sio.loadmat(path2 + file)
     data.pop('__globals__'); data.pop('__header__');  data.pop('__version__')
     sorted(data.keys())
 
     for i, fi in enumerate(data):
-        abnormal_data[count, i*4624: i*4624+4624] = data[fi].reshape([1,4624])
+        abnormal_data[count, i*2278:i*2278+2278] = matrix2vector(data[fi])
+
 np.save('abnormal_data.npy', abnormal_data)
-#
-# # ###########################################
-# 数据标准化：两组数据分别进行标准化 还是放在一起标准化？应该选后者
-# normal_data -= np.mean(normal_data, axis=0)
-# normal_data /= np.std(normal_data, axis=0)
 
-# abnormal_data -= np.mean(abnormal_data, axis=0)
-# abnormal_data /= np.std(abnormal_data, axis=0)
+# ############################################
 
-x = np.ndarray([117, 32368], dtype=np.complex128)
+x = np.ndarray([117, 15946], dtype=np.complex128)
 x[:57, :] = normal_data
 x[57:, :] = abnormal_data
 
@@ -61,19 +71,14 @@ x_abs   = np.abs(x)
 y = np.ndarray([117,1])
 y[:57] = np.ones([57,1])
 y[57:] = np.ones([60,1]) - 2
-#
-# X = pd.DataFrame(x)
-# Y = pd.DataFrame(y)
-# X.label = Y
-# shuffle
 
-# ############################# 模型搭建 #################
 
 # ######################使用sigmoid函数进行分类######################
 
 import sklearn
 from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x_abs, y, test_size=0.3, random_state=0)
+input_data = x_abs
+x_train, x_test, y_train, y_test = train_test_split(input_data, y, test_size=0.3, random_state=1)
 
 # 对训练数据进行标准化
 from sklearn.preprocessing import StandardScaler
